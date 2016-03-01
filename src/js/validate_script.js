4e6695a240e5e4ea9b5d4710880e47e0712dfb3e
Art Garm <artem.garmatyk@gmail.com>
2016-03-01 11:08:22 +0200

    add

--- src/js/validate_script.js
+++ src/js/validate_script.js
@@ -212,6 +212,7 @@ function fancyboxForm(){
 
 $(document).ready(function(){
    validate('#call-popup .contact-form', {submitFunction:validationCall});
+   validate('.form-in-page ', {submitFunction:validationCall});
    Maskedinput();
    fancyboxForm();
 });
