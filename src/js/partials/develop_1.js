c36c9b846190e9047ee4df13b9f54bcaba72625a
Dmitry <dmitry1939@gmail.com>
2016-03-01 14:44:18 +0200

    added contacts & fix for breadcrumbs

--- src/js/partials/develop_1.js
+++ src/js/partials/develop_1.js
@@ -1,6 +1,44 @@
 try{
 
+    // contact map init
+
+    function contactsMapInit(mapWrap){
+            if($('#'+mapWrap).length){
+                function initialize() {
+                    var myLatlng = new google.maps.LatLng(cordX,cordY);
+                    var myOptions = {
+                        zoom: 16,
+                        center: myLatlng,
+                        disableDefaultUI: true,
+                        mapTypeId: google.maps.MapTypeId.ROADMAP,
+                        zoomControlOptions: {
+                           position: google.maps.ControlPosition.LEFT_BOTTOM
+                        }
+                    }
+                    var map = new google.maps.Map(document.getElementById(mapWrap), myOptions);
+
+                    var image = 'images/contact-map-point.png';   // иконка картинкой
+
+                    var marker = new google.maps.Marker({
+                        position: myLatlng,
+                        map: map,
+                        animation: google.maps.Animation.DROP, // анимация при загрузке карты
+                        icon: image //  иконка картинкой
+                    });
+
+                }
+
+                initialize();
+            }
+
+        };
+
+    // /contact map init
+
+
     $(document).ready(function(){
+
+        contactsMapInit('contact-map');
 
     });
 
